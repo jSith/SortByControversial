930d7e5f0e3eaff91ced566c581839c729298637
unknown <jwilmasmith@gmail.com>
2019-03-04 15:12:08 -0600

    Refactor to persist models

--- title_generator.py
+++ title_generator.py
@@ -1,13 +1,41 @@
 import markovify
+import pickle
 
 from api_accessor import get_controversial_posts
 from clean_data import read_file
 
 
-def generate_titles(training_data, num_titles=5):
-    model = markovify.Text(training_data)
+def generate_titles(training_data=None, title=None, num_titles=5):
+    if training_data and title:
+        model = create_model(training_data)
+        save_model(model, title)
+    elif title:
+        model = load_model(title)
+    else:
+        raise ValueError("One of training data or model location expected")
+
     titles = list(model.make_sentence() for i in range(num_titles))
     return titles
+
+
+def create_model(training_data):
+    model = markovify.Text(training_data)
+    return model
+
+
+def save_model(model, title):
+    model_location = f'data\\{title}.sav'
+    with open(model_location, 'wb') as rw_file:
+        pickle.dump(model, rw_file)
+
+
+def load_model(title):
+    model_location = f'data\\{title}.sav'
+    try:
+        with open(model_location, 'wb') as rw_file:
+            return pickle.load(rw_file)
+    except FileNotFoundError:
+        return None
 
 
 def get_data_reddit(subreddit):
@@ -23,7 +51,7 @@ def get_data_file(filename):
 
 if __name__ == '__main__':
     file_location = 'data\\bee_movie_script.txt'
-    trump_speeches = get_data_file(file_location)
-    trump_sentences = generate_titles(trump_speeches, 15)
+    trump_speeches = get_controversial_posts(subreddit="politics")
+    trump_sentences = generate_titles(training_data=trump_speeches, title="r/politics_model")
     generated_speech = ' '.join(trump_sentences)
     print(generated_speech)
